03317819e8854deebd1339f25e5e1ef2e3d4d606
Joonas Häkkinen <joonas@joonashakkinen.fi>
2018-10-12 22:48:13 +0300

    Initial commit

---
+++ src/main/java/fi/basse/tikape/kysymyspankki/Main.java
@@ -0,0 +1,9 @@
+package fi.basse.tikape.kysymyspankki;
+
+
+public class Main {
+    
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, world!");
+    }
+}
