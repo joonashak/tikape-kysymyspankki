91faebfd93c1a84852116820bd374e3c6b570645
Joonas Häkkinen <joonas@joonashakkinen.fi>
2018-10-14 00:38:27 +0300

    Create core Thymeleaf template

--- src/main/java/fi/basse/tikape/kysymyspankki/Main.java
+++ src/main/java/fi/basse/tikape/kysymyspankki/Main.java
@@ -1,9 +1,21 @@
 package fi.basse.tikape.kysymyspankki;
 
+import fi.basse.tikape.kysymyspankki.ui.ModelAndView;
+import java.util.HashMap;
+import spark.Spark;
+import spark.template.thymeleaf.ThymeleafTemplateEngine;
+
 
 public class Main {
     
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, world!");
+        
+        // Public resources (css)
+        Spark.staticFileLocation("/public");
+        
+        Spark.get("/", (req, res) -> {
+            HashMap model = new HashMap();
+            return ModelAndView.createView("questions", model);
+        }, new ThymeleafTemplateEngine());
     }
 }
---
+++ src/main/java/fi/basse/tikape/kysymyspankki/ui/ModelAndView.java
@@ -0,0 +1,12 @@
+package fi.basse.tikape.kysymyspankki.ui;
+
+import java.util.Map;
+
+public class ModelAndView {
+    
+    public static spark.ModelAndView createView(String viewName, Map model) {
+        model.put("viewName", viewName);
+        
+        return new spark.ModelAndView(model, "fragments/template");
+    }
+}
