1a881fa178f01616c63fd3a27b5da2b5f37f2154
Joonas Häkkinen <joonas@joonashakkinen.fi>
2018-10-15 22:29:32 +0300

    Create data validator

---
+++ src/main/java/fi/basse/tikape/kysymyspankki/utils/Validator.java
@@ -0,0 +1,20 @@
+package fi.basse.tikape.kysymyspankki.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Validator {
+  
+  public static Map<String, Boolean> validate(Map<String, String> data) {
+    Map<String, Boolean> results = new HashMap<>();
+    
+    for (Map.Entry<String, String> entry : data.entrySet()) {
+      String key = entry.getKey();
+      String value = entry.getValue();
+      
+      results.put(key, !value.trim().equals(""));
+    }
+    
+    return results;
+  }
+}
