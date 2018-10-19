85f248e14cd1a0b12193e2b4a4ef44bf2bd96005
Joonas Häkkinen <joonas@joonashakkinen.fi>
2018-10-19 17:26:54 +0300

    Port config for heroku

--- src/main/java/fi/basse/tikape/kysymyspankki/Main.java
+++ src/main/java/fi/basse/tikape/kysymyspankki/Main.java
@@ -6,12 +6,18 @@ import fi.basse.tikape.kysymyspankki.controllers.QuestionController;
 import static spark.Spark.get;
 import static spark.Spark.internalServerError;
 import static spark.Spark.notFound;
+import static spark.Spark.port;
 import static spark.Spark.post;
 import static spark.Spark.staticFileLocation;
 
 public class Main {
     
   public static void main(String[] args) throws Exception {
+    
+    if (System.getenv("PORT") != null) {
+            int port = Integer.parseInt(System.getenv("PORT"));
+            port(port);
+        }
 
     // Public resources (css)
     staticFileLocation("/public");
